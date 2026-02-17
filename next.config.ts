bb2e833c80c14b562bfb55ba54920885111c28cd
Ebony <ebony.williams@gmail.com>
2026-02-17 14:16:27 -0500

    Fixes basePath for GitHub Pages static export

--- next.config.ts
+++ next.config.ts
@@ -7,8 +7,7 @@ const nextConfig: NextConfig = {
   // If you want to export to static HTML (for GitHub Pages, etc), uncomment this:
   output: 'export',
   // Only needed for GitHub Pages (production build)
-  basePath: isProd ? '/grow-and-tell' : undefined,
-
+  basePath: isProd ? '/grow-and-tell' : '',
   trailingSlash: true,
   images: {
     unoptimized: true, // Required because GH Pages can't run the Next.js image server
