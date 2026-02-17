063f47c8dcfa97f9d0539781d7c2cdf9b4c2dd30
Ebony <ebony.williams@gmail.com>
2026-02-17 12:31:26 -0500

    Configure static export for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,18 @@
 import type { NextConfig } from 'next';
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   // If you want to export to static HTML (for GitHub Pages, etc), uncomment this:
-  // output: 'export',
-  // assetPrefix: '/product-list-cart/',
-  // basePath: '/product-list-cart',
+  output: 'export',
+  // Only needed for GitHub Pages (production build)
+  basePath: isProd ? '/grow-and-tell' : undefined,
+  assetPrefix: isProd ? '/grow-and-tell/' : undefined,
   trailingSlash: true,
+  images: {
+    unoptimized: true, // Required because GH Pages can't run the Next.js image server
+  },
 };
 
 export default nextConfig;
